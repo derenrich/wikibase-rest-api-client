8ccb21660413b8b7457b92cb4b5ce4296016245c
Daniel Erenrich <daniel@erenrich.net>
2024-01-07 14:47:38 -0800

    support for patch/delete/replace for item/property

--- wikibase_rest_api_client/api/statements/delete_property_statement.py
+++ wikibase_rest_api_client/api/statements/delete_property_statement.py
@@ -1,130 +1,43 @@
-from http import HTTPStatus
-from typing import Any, Dict, Optional, Union
+from typing import Any, Union
 
-import httpx
-
-from ... import errors
 from ...client import AuthenticatedClient, Client
+from ...models import MediawikiEdit
 from ...types import UNSET, Response, Unset
-
-
-def _get_kwargs(
-    property_id: str,
-    statement_id: str,
-    *,
-    if_unmodified_since: Union[Unset, str] = UNSET,
-) -> Dict[str, Any]:
-    headers: Dict[str, Any] = {}
-    if not isinstance(if_unmodified_since, Unset):
-        headers["If-Unmodified-Since"] = if_unmodified_since
-
-    _kwargs: Dict[str, Any] = {
-        "method": "delete",
-        "url": "/entities/properties/{property_id}/statements/{statement_id}".format(
-            property_id=property_id,
-            statement_id=statement_id,
-        ),
-    }
-
-    _kwargs["headers"] = headers
-    return _kwargs
-
-
-def _parse_response(*, client: Union[AuthenticatedClient, Client], response: httpx.Response) -> Optional[Any]:
-    if response.status_code == HTTPStatus.OK:
-        return None
-    if response.status_code == HTTPStatus.BAD_REQUEST:
-        return None
-    if response.status_code == HTTPStatus.NOT_FOUND:
-        return None
-    if response.status_code == HTTPStatus.PRECONDITION_FAILED:
-        return None
-    if response.status_code == HTTPStatus.UNSUPPORTED_MEDIA_TYPE:
-        return None
-    if response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR:
-        return None
-    if client.raise_on_unexpected_status:
-        raise errors.UnexpectedStatus(response.status_code, response.content)
-    else:
-        return None
-
-
-def _build_response(*, client: Union[AuthenticatedClient, Client], response: httpx.Response) -> Response[Any]:
-    return Response(
-        status_code=HTTPStatus(response.status_code),
-        content=response.content,
-        headers=response.headers,
-        parsed=_parse_response(client=client, response=response),
-    )
+from .delete_statement import asyncio_detailed as generic_asyncio_detailed
+from .delete_statement import sync_detailed as generic_sync_detailed
 
 
 def sync_detailed(
     property_id: str,
     statement_id: str,
+    edit: Union[Unset, MediawikiEdit] = UNSET,
     *,
     client: Union[AuthenticatedClient, Client],
     if_unmodified_since: Union[Unset, str] = UNSET,
 ) -> Response[Any]:
-    """Delete a single Statement from a Property
-
-     This endpoint is also accessible through `/statements/{statement_id}`.
-
-    Args:
-        property_id (str):
-        statement_id (str):
-        if_unmodified_since (Union[Unset, str]):
-
-    Raises:
-        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
-        httpx.TimeoutException: If the request takes longer than Client.timeout.
-
-    Returns:
-        Response[Any]
-    """
-
-    kwargs = _get_kwargs(
-        property_id=property_id,
+    if not statement_id.startswith(property_id):
+        raise ValueError("statement_id must match the passed property_id")
+    return generic_sync_detailed(
         statement_id=statement_id,
+        edit=edit,
+        client=client,
         if_unmodified_since=if_unmodified_since,
     )
-
-    response = client.get_httpx_client().request(
-        **kwargs,
-    )
-
-    return _build_response(client=client, response=response)
 
 
 async def asyncio_detailed(
     property_id: str,
     statement_id: str,
+    edit: Union[Unset, MediawikiEdit] = UNSET,
     *,
     client: Union[AuthenticatedClient, Client],
     if_unmodified_since: Union[Unset, str] = UNSET,
 ) -> Response[Any]:
-    """Delete a single Statement from a Property
-
-     This endpoint is also accessible through `/statements/{statement_id}`.
-
-    Args:
-        property_id (str):
-        statement_id (str):
-        if_unmodified_since (Union[Unset, str]):
-
-    Raises:
-        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
-        httpx.TimeoutException: If the request takes longer than Client.timeout.
-
-    Returns:
-        Response[Any]
-    """
-
-    kwargs = _get_kwargs(
-        property_id=property_id,
+    if not statement_id.startswith(property_id):
+        raise ValueError("statement_id must match the passed property_id")
+    return generic_asyncio_detailed(
         statement_id=statement_id,
+        edit=edit,
+        client=client,
         if_unmodified_since=if_unmodified_since,
     )
-
-    response = await client.get_async_httpx_client().request(**kwargs)
-
-    return _build_response(client=client, response=response)
